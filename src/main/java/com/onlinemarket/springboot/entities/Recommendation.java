1b716f0d60fe2fc78e1b4a393c835918abc348ce
shohamD1121 <shohamdouek@gmail.com>
2023-02-25 19:29:40 +0200

    fixed some issues with columns naming

--- src/main/java/com/onlinemarket/springboot/entities/Recommendation.java
+++ src/main/java/com/onlinemarket/springboot/entities/Recommendation.java
@@ -3,10 +3,14 @@ package com.onlinemarket.springboot.entities;
 import java.security.Timestamp;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "recommendations")
 public class Recommendation {
     
     @Id
