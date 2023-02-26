8ccba9c6f58516cff9a208258ef9cf41a851aa23
shohamD1121 <shohamdouek@gmail.com>
2023-02-26 16:23:15 +0200

    added password hashing

--- src/main/java/com/onlinemarket/springboot/services/UserService.java
+++ src/main/java/com/onlinemarket/springboot/services/UserService.java
@@ -3,21 +3,25 @@ package com.onlinemarket.springboot.services;
 import com.onlinemarket.springboot.dto.UserDto;
 import com.onlinemarket.springboot.entities.User;
 import com.onlinemarket.springboot.repositories.UserRepository;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     
-   private final UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(UserDto userDto){
         User user = new User();
         user.setUsername(userDto.getUsername());
-        user.setPassword(userDto.getPassword());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
         user.setEmail(userDto.getEmail());
         user.setFirstName(userDto.getFirstName());
         user.setLastName(userDto.getLastName());
