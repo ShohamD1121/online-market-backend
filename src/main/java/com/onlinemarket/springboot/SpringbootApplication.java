5e56de4884a694496005f5a435756b37ea011c03
shohamD1121 <shohamdouek@gmail.com>
2023-02-26 19:14:25 +0200

    added a logger

--- src/main/java/com/onlinemarket/springboot/SpringbootApplication.java
+++ src/main/java/com/onlinemarket/springboot/SpringbootApplication.java
@@ -2,19 +2,25 @@ package com.onlinemarket.springboot;
 
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class SpringbootApplication {
+
+	public static final Logger logger = LogManager.getLogger(SpringbootApplication.class);
 
 	public static void main(String[] args) {
 
 
 		Map<String, String> envVars = System.getenv();
 		envVars.forEach((key, value) -> System.setProperty(key, value));
-
 		SpringApplication.run(SpringbootApplication.class, args);
+		
 	}
 
 }
